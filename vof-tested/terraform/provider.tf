ade4a5065a40a0c55ff0135e8c00fa3f89a2100b
FlevianK <flevian.kanaiza@andela.com>
2017-08-23 17:13:25 +0300

    Add files to create vof infrastructure

---
+++ vof-tested/terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  "credentials" = "${file("/Users/fleviankanaiza/DevOps/vof-tested/Testbed.json")}"
+  "project"     = "vof-testbed-2"
+  "region"      = "${var.region}"
+}
---
+++ vof-tested/terraform/vof.tf
@@ -0,0 +1,15 @@
+variable "env_name" {
+  type = "string"
+}
+
+variable "region" {
+  type = "string"
+}
+
+variable "google_project_id" {
+  type = "string"
+}
+
+variable "ip_cidr_range" {
+  type = "string"
+}
